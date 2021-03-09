9749b3eefff2fa09d763cc460e260ff62b7bc69a
Mark Robert Bumatay <kramtrebor1995@gmail.com>
2021-03-09 21:25:40 +0800

    try bootstrap 4

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-const navbar = document.getElementById("mainNavbar");
-document.addEventListener("scroll", () => {
-  if (document.documentElement.scrollTop > navbar.clientHeight) {
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
-});
-
-AOS.init({ duration: 1000 });
-
-const completedTab = document.getElementById("pills-completed-tab");
-const inProgressTab = document.getElementById("pills-inprogress-tab");
-
-completedTab.addEventListener("click", function () {
-  setTimeout(function () {
-    AOS.refreshHard();
-  }, 500);
-});
-
-inProgressTab.addEventListener("click", function () {
-  setTimeout(function () {
-    AOS.refreshHard();
-  }, 500);
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("#mainNavbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
 });
