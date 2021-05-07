fb4b2dcc99ab9ac5beede14fbcdc1d90023efd07
Mark Robert Bumatay <kramtrebor1995@gmail.com>
2021-05-07 15:03:04 +0800

    add new courses and change card background color

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ $(function () {
     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
   });
 });
+
+AOS.init({ duration: 1000 });
+
+const completedTab = document.getElementById("pills-completed-tab");
+const inProgressTab = document.getElementById("pills-inprogress-tab");
+
+completedTab.addEventListener("click", function () {
+  setTimeout(function () {
+    AOS.refreshHard();
+  }, 500);
+});
+
+inProgressTab.addEventListener("click", function () {
+  setTimeout(function () {
+    AOS.refreshHard();
+  }, 500);
+});
